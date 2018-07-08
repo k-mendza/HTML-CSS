1acd8f15f89a9c51be840903d6c95501a8ade6d2
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-08 23:45:43 +0200

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,17 @@
+function myFunction(){
+    console.log("This is myFunction START");
+    console.log("This is myFunction END");
+}
+
+function square(num){
+    console.log(num*num);
+}
+
+var number1 = 10;
+var number2 = 32;
+function sumOfTwo(num1, num2){
+    console.log("First argument is: " + num1);
+    console.log("Second argument is: " + num2);
+    console.log("The sum of arguments is: " + (num1+num2));
+    return num1+num2;
+}
