67f7431d6673b18102bb643b3941e4a657eaadae
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-12 21:36:59 +0200

    Add files via upload

--- fun.js
+++ fun.js
@@ -23,3 +23,12 @@ function isEven(num){
         return false;
     }
 }
+
+function factorial(num){
+    var result = 1;
+    for (var i = 1; i <= num;i++){
+        result = result*i;
+    }
+    return result;
+}
+
