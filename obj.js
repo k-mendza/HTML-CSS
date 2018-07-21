8c28979cdd3505f01660eaed052f28cb39a8d393
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-21 11:46:01 +0200

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,9 @@
+var myObject = {
+    fieldOne: "value1",
+    fieldTwo: "0",
+    fieldThree: "value3"
+};
+
+console.log(myObject.fieldOne);
+console.log(myObject.fieldTwo);
+console.log(myObject.fieldThree);
