507ce49371addf046c35642d8e0df0d32b192407
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-25 20:02:48 +0200

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -1,2 +1,10 @@
 var h1 = document.querySelector("h1");
 h1.style.color = "pink";
+
+var redVar = document.getElementsByClassName("red");
+redVar.style.color = "red";
+
+var border = document.getElementById("border");
+border.style.border = "1px solid black";
+
+
