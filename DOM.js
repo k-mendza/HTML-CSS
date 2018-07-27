b2cfe5d3c1e9f65590cc1f9d8df7d762b9fc8f19
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-27 23:55:40 +0200

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -7,5 +7,8 @@ h1.style.color = "pink";
 var h1 = document.querySelector("#border");
 h1.style.border = "1px solid black" ;
 
+var img1 = document.querySelector("img");
+img1.setAttribute("src","https://luxway.pl/wp-content/uploads/2017/11/Ducati_6.png?x85208")
+
 
 
