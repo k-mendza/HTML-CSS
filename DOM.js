c4f426a5a00bfdda750d24eff0e6587ba2b73ff1
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-29 15:44:08 +0200

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -10,5 +10,20 @@ h1.style.border = "1px solid black" ;
 var img1 = document.querySelector("img");
 img1.setAttribute("src","https://luxway.pl/wp-content/uploads/2017/11/Ducati_6.png?x85208")
 
+var header = document.querySelector("h1");
+header.addEventListener("click", function(){
+    alert("Some1 has clicked on header!")
+})
+header.addEventListener("click", function(){
+   header.style.backgroundColor = "blue";
+})
 
-
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+var isPurple = false;
+button.addEventListener("click",function(){
+    if(isPurple === false){
+        body.style.backgroundColor = "purple";
+        isPurple = true;
+    }
+})
