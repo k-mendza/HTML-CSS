0aade35029cae8f2e13f3db4f4fc5654cf2c4305
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-07-31 23:47:50 +0200

    Add files via upload

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -1,4 +1,3 @@
-var h1 = document.querySelector("h1");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 
@@ -16,24 +15,29 @@ p1Button.addEventListener("click",function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === scoreLimit) {
+            p1Display.classList.add("winner");
             gameOver = true;
         }
+        p1Display.textContent = p1Score;
     }
-    p1Display.textContent = p1Score;
 });
 p2Button.addEventListener("click",function(){
     if(!gameOver){
         p2Score++;
         if(p2Score === scoreLimit) {
             gameOver = true;
+            p2Display.classList.add("winner");
         }
+        p2Display.textContent = p2Score;
     }
-    p2Display.textContent = p2Score;
 });
+
 resetButton.addEventListener("click",function(){
     var p1Score = 0;
     var p2Score = 0;
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
     gameOver = false;
 });
